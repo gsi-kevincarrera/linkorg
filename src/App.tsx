fa292006a458277b9b3ff903131f422f93544edc
Kevin Carrera <kevin.cc@factory.generalsoftwareinc.com>
2025-02-27 01:31:11 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-white text-7xl'>
+      hi
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { scan } from 'react-scan' // must be imported before React and React DOM
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+scan({
+  enabled: true,
+})
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
