fb9c971f5b3f4927b9392d90555ad1bf707daddd
Kevin Carrera <kevin.cc@factory.generalsoftwareinc.com>
2025-02-28 02:37:36 -0500

    refactor: remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { scan } from 'react-scan' // must be imported before React and React DOM
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -9,7 +8,5 @@ scan({
 })
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <App />
-  </StrictMode>
 )
