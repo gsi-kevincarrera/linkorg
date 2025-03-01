79942b74f406e7272b8302946d2b1705786cb9fd
Kevin Carrera <kevin.cc@factory.generalsoftwareinc.com>
2025-02-28 23:59:02 -0500

    refactor: remove unnecesary suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from 'react'
 import './App.css'
 import Board from './components/board'
 import Navbar from './components/navigation/navbar'
@@ -6,17 +5,13 @@ import ErrorBoundary from './components/error-boundary'
 import { Toaster } from '@/components/ui/sonner'
 
 function App() {
-  
-
   return (
     <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
       <Navbar />
       <ErrorBoundary fallback={<div>Error</div>}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Board />
-        </Suspense>
+        <Board />
       </ErrorBoundary>
-      <Toaster  richColors position='top-center' theme='light'/>
+      <Toaster richColors position='top-center' theme='light' />
     </div>
   )
 }
